c7adb47d0dfc366ed11518b9394a1e9a57fe176e
liurui <lovearuis@gmail.com>
2023-11-09 15:33:21 +0800

    Initial Commit

---
+++ TimeBank/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TimeBank
+//
+//  Created by 牧云踏歌 on 2023/11/9.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
